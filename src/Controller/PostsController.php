fc637d29162620cd3e3b74c7644c814771e3f184
Abril Pascual Avila <aprilpascual1125@gmail.com>
2025-11-14 13:46:36 +0100

    Bases del post

---
+++ src/Controller/PostsController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class PostsController extends AbstractController
+{
+    #[Route('/Registrar-Posts', name: 'RegistrarPosts')]
+    public function index(Request $request)
+    {
+        $post = new Posts();
+        $form = $this->createForm(PostsType::class, $post);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+
+        return $this->render('posts/index.html.twig', [
+            'controller_name' => 'PostsController',
+        ]);
+    }
+}
---
+++ src/Entity/Posts.php
@@ -0,0 +1,127 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PostsRepository;
+use DateTime;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: PostsRepository::class)]
+class Posts
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $title = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $leaf = null;
+
+    #[ORM\Column(length: 255 , nullable: true)]
+    private ?string $photo = null;
+
+    #[ORM\Column]
+    private ?\DateTime $fecha_publicacion = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $contenido = null;
+
+    #[ORM\Column]
+    private ?int $user_id = null;
+
+    // Constructor
+    public function __construct()
+    {
+        $this->leaf ='';
+        $this->fecha_publicacion = new \DateTime;
+    }
+
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function setId(string $id): static
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): static
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getLeaf(): ?string
+    {
+        return $this->leaf;
+    }
+
+    public function setLeaf(string $leaf): static
+    {
+        $this->leaf = $leaf;
+
+        return $this;
+    }
+
+    public function getphoto(): ?string
+    {
+        return $this->photo;
+    }
+
+    public function setphoto(string $photo): static
+    {
+        $this->photo = $photo;
+
+        return $this;
+    }
+
+    public function getFechaPublicacion(): ?\DateTime
+    {
+        return $this->fecha_publicacion;
+    }
+
+    public function setFechaPublicacion(\DateTime $fecha_publicacion): static
+    {
+        $this->fecha_publicacion = $fecha_publicacion;
+
+        return $this;
+    }
+
+    public function getContenido(): ?string
+    {
+        return $this->contenido;
+    }
+
+    public function setContenido(string $contenido): static
+    {
+        $this->contenido = $contenido;
+
+        return $this;
+    }
+
+    public function getUserId(): ?int
+    {
+        return $this->user_id;
+    }
+
+    public function setUserId(int $user_id): static
+    {
+        $this->user_id = $user_id;
+
+        return $this;
+    }
+}
