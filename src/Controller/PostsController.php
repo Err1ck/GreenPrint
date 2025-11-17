932d94e6ef87ba1ea94c3b3886270df1f0c7f397
Abril Pascual Avila <aprilpascual1125@gmail.com>
2025-11-17 14:21:04 +0100

    Actualizar PostsController

--- src/Controller/PostsController.php
+++ src/Controller/PostsController.php
@@ -6,18 +6,18 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-final class PostsController extends AbstractController
-{
-    #[Route('/Registrar-Posts', name: 'RegistrarPosts')]
-    public function index(Request $request)
-    {
-        $post = new Posts();
-        $form = $this->createForm(PostsType::class, $post);
-        $form->handleRequest($request);
-        if ($form->isSubmitted() && $form->isValid()) {
+// final class PostsController extends AbstractController
+// {
+//     #[Route('/Registrar-Posts', name: 'RegistrarPosts')]
+//     public function index(Request $request)
+//     {
+//         $post = new Posts();
+//         $form = $this->createForm(PostsType::class, $post);
+//         $form->handleRequest($request);
+//         if ($form->isSubmitted() && $form->isValid()) {
 
-        return $this->render('posts/index.html.twig', [
-            'controller_name' => 'PostsController',
-        ]);
-    }
-}
+//         return $this->render('posts/index.html.twig', [
+//             'controller_name' => 'PostsController',
+//         ]);
+//     }
+// }
