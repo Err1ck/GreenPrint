d364e92b650b9b54fa2d5974f3cdb4d5ac772cda
Abril Pascual Avila <aprilpascual1125@gmail.com>
2025-11-20 13:51:10 +0100

    Fix db and variable

--- src/Controller/PostsController.php
+++ src/Controller/PostsController.php
@@ -8,29 +8,29 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Doctrine\ORM\EntityManagerInterface;
 
-// final class PostsController extends AbstractController
-// {
-//     #[Route('/Registrar-Posts', name: 'RegistrarPosts')]
-//     public function index(Request $request): Response
-//     {
-//         $post = new Posts();
-//         $form = $this->createForm(PostsType::class, $post);
-//         $form->handleRequest($request);
+final class PostsController extends AbstractController
+{
+    #[Route('/Registrar-Posts', name: 'RegistrarPosts')]
+    public function index(Request $request, EntityManagerInterface $em): Response
+    {
+        $post = new Posts();
+        $form = $this->createForm(PostsType::class, $post);
+        $form->handleRequest($request);
 
-//         if ($form->isSubmitted() && $form->isValid()) {
-//             $user = $this->getUser();
-//             $post->setUser($user);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $username = $this->getUser();
+            $post->setUseranme($username);
 
-//             $em = $this->getDoctrine()->getManager();
-//             $em->persist($post);
-//             $em->flush();
+            $em->persist($post);
+            $em->flush();
 
-//             return $this->redirectToRoute('dashboard');
-//         }
+            return $this->redirectToRoute('dashboard');
+        }
 
-//         return $this->render('posts/index.html.twig', [
-//             'form' => $form->createView(),
-//         ]);
-//     }
-// }
+        return $this->render('posts/index.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+}
--- src/Entity/Posts.php
+++ src/Entity/Posts.php
@@ -1,133 +1,113 @@
 <?php
 
-// namespace App\Entity;
+namespace App\Entity;
 
+use App\Repository\PostsRepository;
+use App\Entity\User;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
 
-// use App\Repository\PostsRepository;
-// use App\Entity\User;
-// use DateTime;
-// use Doctrine\DBAL\Types\Types;
-// use Doctrine\ORM\Mapping as ORM;
+#[ORM\Entity(repositoryClass: PostsRepository::class)]
+class Posts
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
 
-// #[ORM\Entity(repositoryClass: PostsRepository::class)]
-// class Posts
-// {
-//     #[ORM\Id]
-//     #[ORM\GeneratedValue]
-//     #[ORM\Column]
-//     private ?int $id = null;
+    #[ORM\ManyToOne(targetEntity: User::class)]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?User $username = null;
 
-//     #[ORM\Column(length: 255)]
-//     private ?string $title = null;
+    #[ORM\Column(length: 255)]
+    private ?string $title = null;
 
-//     #[ORM\Column(length: 255, nullable: true)]
-//     private ?string $leaf = null;
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $leaf = null;
 
-//     #[ORM\Column(length: 255 , nullable: true)]
-//     private ?string $photo = null;
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $photo = null;
 
-//     #[ORM\Column]
-//     private ?\DateTime $fecha_publicacion = null;
+    #[ORM\Column]
+    private ?\DateTimeInterface $fecha_publicacion = null;
 
-//     #[ORM\Column(type: Types::TEXT)]
-//     private ?string $contenido = null;
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $contenido = null;
 
-//     #[ORM\ManyToOne(targetEntity: User::class)]
-//     #[ORM\JoinColumn(nullable: false)]
-//     private ?User $user = null;
+    public function __construct()
+    {
+        $this->leaf = '';
+        $this->fecha_publicacion = new \DateTime();
+    }
 
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
 
-//     // Constructor
-//     public function __construct()
-//     {
-//         $this->leaf ='';
-//         $this->fecha_publicacion = new \DateTime;
-//     }
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
 
+    public function setTitle(string $title): static
+    {
+        $this->title = $title;
+        return $this;
+    }
 
-//     public function getId(): ?int
-//     {
-//         return $this->id;
-//     }
+    public function getLeaf(): ?string
+    {
+        return $this->leaf;
+    }
 
-//     public function setId(string $id): static
-//     {
-//         $this->id = $id;
+    public function setLeaf(?string $leaf): static
+    {
+        $this->leaf = $leaf;
+        return $this;
+    }
 
-//         return $this;
-//     }
+    public function getPhoto(): ?string
+    {
+        return $this->photo;
+    }
 
-//     public function getTitle(): ?string
-//     {
-//         return $this->title;
-//     }
+    public function setPhoto(?string $photo): static
+    {
+        $this->photo = $photo;
+        return $this;
+    }
 
-//     public function setTitle(string $title): static
-//     {
-//         $this->title = $title;
+    public function getFechaPublicacion(): ?\DateTimeInterface
+    {
+        return $this->fecha_publicacion;
+    }
 
-//         return $this;
-//     }
+    public function setFechaPublicacion(\DateTimeInterface $fecha_publicacion): static
+    {
+        $this->fecha_publicacion = $fecha_publicacion;
+        return $this;
+    }
 
-//     public function getLeaf(): ?string
-//     {
-//         return $this->leaf;
-//     }
+    public function getContenido(): ?string
+    {
+        return $this->contenido;
+    }
 
-//     public function setLeaf(string $leaf): static
-//     {
-//         $this->leaf = $leaf;
+    public function setContenido(string $contenido): static
+    {
+        $this->contenido = $contenido;
+        return $this;
+    }
 
-//         return $this;
-//     }
+    public function getUsername(): ?User
+    {
+        return $this->username;
+    }
 
-//     public function getphoto(): ?string
-//     {
-//         return $this->photo;
-//     }
-
-//     public function setphoto(string $photo): static
-//     {
-//         $this->photo = $photo;
-
-//         return $this;
-//     }
-
-//     public function getFechaPublicacion(): ?\DateTime
-//     {
-//         return $this->fecha_publicacion;
-//     }
-
-//     public function setFechaPublicacion(\DateTime $fecha_publicacion): static
-//     {
-//         $this->fecha_publicacion = $fecha_publicacion;
-
-//         return $this;
-//     }
-
-//     public function getContenido(): ?string
-//     {
-//         return $this->contenido;
-//     }
-
-//     public function setContenido(string $contenido): static
-//     {
-//         $this->contenido = $contenido;
-
-//         return $this;
-//     }
-
-//     public function getUser(): ?User
-//     {
-//         return $this->user;
-//     }
-
-//     public function setUser(User $user): static
-//     {
-//         $this->user = $user;
-//         return $this;
-//     }
-
-
-// }
-
+    public function setUseranme(User $username): static
+    {
+        $this->username = $username;
+        return $this;
+    }
+}
