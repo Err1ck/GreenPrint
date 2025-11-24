116227d277797c2b9cd25f1693701c760b3fcbad
Erick Cabezas <erryck-1996@hotmail.com>
2025-11-24 12:07:28 +0100

    feat: Navbar.jsx

--- assets/componentes/common/Navbar.jsx
+++ assets/componentes/common/Navbar.jsx
@@ -1,5 +1,14 @@
+import SvgComponente from "../ui/Svg";
+
 function Navbar() {
-    return <div></div>;
+    return (
+        <nav>
+            <SvgComponente></SvgComponente>
+            <div>
+                
+            </div>
+        </nav>
+    );
 }
 
 export default Navbar;
