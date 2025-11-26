1fc938e0c369001ab4e344c1fa68c75613b654ad
Erick Cabezas <erryck-1996@hotmail.com>
2025-11-26 10:38:35 +0100

    refactor: svg.jsx, app.css

--- assets/componentes/ui/Svg.jsx
+++ assets/componentes/ui/Svg.jsx
@@ -1,13 +1,49 @@
+import Icono1 from "./img/icon1.svg";
+import Icono2 from "./img/icon2.svg";
+import Icono3 from "./img/icon3.svg";
+import Icono4 from "./img/icon4.svg";
+import Icono5 from "./img/icon5.svg";
+import Icono6 from "./img/icon6.svg";
+import Icono7 from "./img/icon7.svg";
+import Icono8 from "./img/icon8.svg";
+import Icono9 from "./img/icon9.svg";
+import Icono10 from "./img/icon10.svg";
+
 /**
  *
- * @param img_class Clase del svg o imagen.
- * @param img_src Pathing del svg o imagen.
- * @param img_alt Descripcion del svg o imagen.
- * @returns
  */
 
-function SvgComponente(img_class, img_src, img_alt) {
-    return <img className={img_class} src={img_src} alt={img_alt} />;
+const RECURSOS = {
+    svg: {
+        icon1: Icono1,
+        icon2: Icono2,
+        icon3: Icono3,
+        icon4: Icono4,
+        icon5: Icono5,
+        icon6: Icono6,
+        icon7: Icono7,
+        icon8: Icono8,
+        icon9: Icono9,
+        icon10: Icono10,
+    },
+    img: {
+        imagen1: Imagen1,
+        imagen2: Imagen2,
+    },
+};
+
+function SvgComponente({ name, className }) {
+    const SvgIcon = RECURSOS.svg[name];
+    if (SvgIcon) {
+        return <SvgIcon role="img" className={className} />;
+    }
+    const imagePath = RECURSOS.img[name];
+    if (imagePath) {
+        return <img src={imagePath} className={className} />;
+    }
+
+    console.error(`Recurso no encontrado: ${name}`);
+    return null;
 }
 
 export default SvgComponente;
